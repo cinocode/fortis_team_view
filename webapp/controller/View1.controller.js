f578a118c7b98deaeb4549b324ccb9e6bc119ceb
Laut Moritz <moritz.lautenschlager@fortis-it.de>
2019-03-13 14:43:08 +0000

    hello world button

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,11 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
-
 	return Controller.extend("fortis_team_view.controller.View1", {
-
+		/**
+		 *@memberOf fortis_team_view.controller.View1
+		 */
+		button_clicked: function (oEvent) {
+			alert("Hello World");
+		}
 	});
-
 });
